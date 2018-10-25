c9523034e972e85c3885dc006a4d920f6a9ee95c
Gracjan <graku12@gmail.com>
2018-10-25 15:59:01 +0200

    New one ! Add RecyclerView

---
+++ Application/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Application
+{
+    
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.LayoutManager mLayoutManager;
+        private RecyclerView.Adapter mAdapter;
+        
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            
+
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recycler_view);
+            List<Title> mTitles = new List<Title>();
+            mTitles.Add(new Application.Title() { Name = "Barcelona" });
+            mTitles.Add(new Application.Title() { Name = "Madryt" });
+            mTitles.Add(new Application.Title() { Name = "Warszawa" });
+            mTitles.Add(new Application.Title() { Name = "Poznań" });
+            mTitles.Add(new Application.Title() { Name = "Kraków" });
+            mTitles.Add(new Application.Title() { Name = "Wałbrzych" });
+            mTitles.Add(new Application.Title() { Name = "Wrocław" });
+            mTitles.Add(new Application.Title() { Name = "Ciechocinek" });
+            mTitles.Add(new Application.Title() { Name = "New York" });
+            mTitles.Add(new Application.Title() { Name = "Oslo" });
+            mTitles.Add(new Application.Title() { Name = "Berlin" });
+            mTitles.Add(new Application.Title() { Name = "Gdańsk" });
+
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+            mAdapter = new TitleAdapter(mTitles);
+            mRecyclerView.SetAdapter(mAdapter);
+        }
+    }
+}
---
+++ Application/TitleAdapter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+
+namespace Application
+{
+    public class TitleAdapter : RecyclerView.Adapter
+    {
+        private List<Application.Title> titles = new List<Title>();
+
+        public TitleAdapter(List<Application.Title> mTitles)
+        {
+            titles = mTitles;
+        }
+
+       
+
+        class MyView : RecyclerView.ViewHolder
+        {
+            public View mMainView;
+            public TextView classTitle;
+
+            public MyView(View view) : base(view)
+            {
+                mMainView = view;
+            }
+
+           
+        }
+
+        public override int ItemCount
+        {
+            get
+            {
+                return titles.Count;
+            }
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            MyView myHolder = holder as MyView;
+            myHolder.classTitle.Text = titles[position].Name;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View article = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.article_layout, parent,false);
+            TextView mName = article.FindViewById<TextView>(Resource.Id.article_title);
+            MyView view = new MyView(article) { classTitle = mName };
+            return view;
+        }
+    }
+}
